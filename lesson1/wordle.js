ff0edf37ab7f9ff96dde6f0688cc99a98c1e7af3
Harrison Lau <harrison.lau@cloverhealth.com>
2023-05-04 22:56:43 +0800

    lesson1 touchup

--- lesson1/wordle.js
+++ lesson1/wordle.js
@@ -3,13 +3,13 @@ const wordList = ["apple", "paper", "melon", "zebra", "books", "cheap"];
 function startGame(round) {
     let attempt = 0;
     while (attempt < round) {
-        let userInput = prompt("Guess a five letter word: ");
+        let currentGuess = prompt("Guess a five letter word: ");
         // 1. Check if word is in word list
-        if (validateInput(userInput)) {
-            console.log(userInput);
+        if (validateInput(currentGuess)) {
+            console.log(currentGuess);
             attempt = attempt + 1;
         } else {
-            retry(userInput);
+            retry(currentGuess);
         }
     }
 }
